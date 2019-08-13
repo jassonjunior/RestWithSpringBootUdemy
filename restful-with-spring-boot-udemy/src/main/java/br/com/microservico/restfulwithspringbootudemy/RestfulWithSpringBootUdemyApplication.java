f94fa4934163377f1b12243ef2b283f162a4efba
Jasson Dantas <JDantas@raiadrogasil.com>
2019-08-13 13:45:35 -0300

    Subindo projeto

---
+++ restful-with-spring-boot-udemy/src/main/java/br/com/microservico/restfulwithspringbootudemy/RestfulWithSpringBootUdemyApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservico.restfulwithspringbootudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWithSpringBootUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWithSpringBootUdemyApplication.class, args);
+	}
+
+}
---
+++ restful-with-spring-boot-udemy/src/test/java/br/com/microservico/restfulwithspringbootudemy/RestfulWithSpringBootUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microservico.restfulwithspringbootudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWithSpringBootUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
